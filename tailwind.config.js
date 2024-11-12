7658be1d46af94833fa88147a6834152338dc551
farhanisty <farhannivtasija@gmail.com>
2024-11-12 17:36:29 +0700

    set primary and secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#006C67",
+        secondary: "#FFB100",
+      },
+    },
   },
   plugins: [],
 };
