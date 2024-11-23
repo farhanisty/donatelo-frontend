af0f06e66c7feb2914cfad038a943a28c6cfcaeb
farhanisty <farhannivtasija@gmail.com>
2024-11-24 01:29:30 +0700

    create ProductBox 3 item per row

--- src/components/ProductBox.jsx
+++ src/components/ProductBox.jsx
@@ -3,8 +3,10 @@ import { VscDiffAdded } from "react-icons/vsc";
 export default function ProductBox({ name, price, image }) {
   const baseImageUrl = import.meta.env.VITE_BASE_IMAGE_URL;
   return (
-    <div className="bg-secondary hover:opacity-70 flex-1 p-4 flex flex-col items-center rounded">
-      <h1 className="text-center font-bold uppercase text-xl">{name}</h1>
+    <div className="bg-secondary flex-[0_0_30%] p-4 flex flex-col items-center rounded">
+      <h1 className="text-center font-bold uppercase text-xl cursor-pointer hover:underline">
+        {name}
+      </h1>
       <img
         className="my-10 object-fit w-[150px] h-[150px]"
         src={`${baseImageUrl}/${image}`}
@@ -14,7 +16,13 @@ export default function ProductBox({ name, price, image }) {
       />
       <div className="w-full flex justify-between">
         <h2>Rp. {price}</h2>
-        <VscDiffAdded className="text-3xl" />
+        <button
+          onClick={() => {
+            console.log("hello world");
+          }}
+        >
+          <VscDiffAdded className="text-3xl hover:scale-110" />
+        </button>
       </div>
     </div>
   );
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,24 @@
 import Navbar from "./../components/Navbar.jsx";
+import Footer from "./../components/Footer.jsx";
 import heroShopImage from "./../assets/hero-shop.jpg";
+import SectionTitle from "./../components/SectionTitle.jsx";
+import Container from "./../layout/Container.jsx";
+import ProductBox from "./../components/ProductBox.jsx";
+import { useQuery } from "react-query";
+import { useState } from "react";
+import { getDonuts } from "./../repository/donut.js";
+
+function splitArray(array, maxItems) {
+  const result = [];
+  for (let i = 0; i < array.length; i += maxItems) {
+    result.push(array.slice(i, i + maxItems));
+  }
+  return result;
+}
 
 export default function Shop() {
+  const { data: donuts, isLoading } = useQuery("getDonuts", getDonuts);
+
   return (
     <>
       <Navbar />
@@ -30,7 +47,29 @@ export default function Shop() {
           </div>
         </section>
       </div>
-      <div className="w-screen min-h-screen"></div>
+      <section className="w-screen min-h-screen bg-background">
+        <Container>
+          <SectionTitle title="PRODUCTS" />
+          <div className="mt-10">
+            {!isLoading ? (
+              <ul className="w-full flex flex-wrap flex-col md:flex-row  gap-5">
+                {donuts.map((donut) => (
+                  <ProductBox
+                    name={donut.name}
+                    key={donut.id}
+                    price={donut.price}
+                    image={donut.image}
+                  />
+                ))}
+              </ul>
+            ) : (
+              <h1>Lodaing</h1>
+            )}
+          </div>
+        </Container>
+      </section>
+
+      <Footer />
     </>
   );
 }
