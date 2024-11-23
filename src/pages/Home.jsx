2e8872d854860ffd396ec85e161b98ee0f3933fa
farhanisty <farhannivtasija@gmail.com>
2024-11-23 22:36:53 +0700

    use react query to perform fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,18 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useQuery } from "react-query";
 import Navbar from "./../components/Navbar.jsx";
 import Footer from "./../components/Footer.jsx";
 import HeroSection from "./../components/HeroSection.jsx";
 import SectionTitle from "./../components/SectionTitle.jsx";
 import Container from "./../layout/Container.jsx";
 import ProductBox from "./../components/ProductBox.jsx";
+import { getDonuts } from "./../repository/donut.js";
 
 export default function Home() {
-  const [donuts, setDonuts] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:8005/api/menu").then((res) => {
-      const donuts = res.data.data.slice(0, 3);
-      console.log(donuts);
-      setDonuts(donuts);
-    });
-  }, []);
+  const { data: donuts, error, isLoading } = useQuery("getDonuts", getDonuts);
+
+  if (error) {
+    return <h1>Fetch error: {error.message}</h1>;
+  }
 
   return (
     <>
@@ -30,7 +27,7 @@ export default function Home() {
       </header>
       <section className="min-h-screen bg-background pt-10">
         <Container>
-          <SectionTitle title="Best seller" />
+          <SectionTitle title="Latest Product" />
           <p className="max-w-[400px] pt-10">
             Don&apos;t miss the favorites everyone is raving
             about—Donatelo&apos;s best-sellers are more than just donuts;
@@ -38,16 +35,23 @@ export default function Home() {
           </p>
 
           <div className="mt-10">
-            <ul className="w-full flex flex-col md:flex-row justify-between gap-5">
-              {donuts.map((donut) => (
-                <ProductBox
-                  name={donut.name}
-                  key={donut.id}
-                  price={donut.price}
-                  image={donut.image}
-                />
-              ))}
-            </ul>
+            {!isLoading ? (
+              <ul className="w-full flex flex-col md:flex-row justify-between gap-5">
+                {donuts
+                  .slice(-3)
+                  .reverse()
+                  .map((donut) => (
+                    <ProductBox
+                      name={donut.name}
+                      key={donut.id}
+                      price={donut.price}
+                      image={donut.image}
+                    />
+                  ))}
+              </ul>
+            ) : (
+              <h1>Lodaing</h1>
+            )}
           </div>
         </Container>
       </section>
