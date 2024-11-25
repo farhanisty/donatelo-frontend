9bb2ad114ae1eebcf5e40e77bb06f4cf29e153bb
farhanisty <farhannivtasija@gmail.com>
2024-11-25 12:11:46 +0700

    perform increment and decrement button

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,16 +1,48 @@
 import donutImage from "./../assets/hero-donat.webp";
+import { useDispatch } from "react-redux";
+import { addToCart, decrementCart } from "../redux/slices/cartSlice.js";
 
-export default function CartItem() {
+export default function CartItem({ product, qty }) {
+  const dispatch = useDispatch();
+  const baseImageUrl = import.meta.env.VITE_BASE_IMAGE_URL;
   return (
     <div className="flex gap-5 border-b pb-3">
-      <img className="w-[30%]" src={donutImage} alt="" />
+      <img
+        className="w-[30%]"
+        src={`${baseImageUrl}/${product.image}`}
+        alt="donut image"
+      />
       <div className="flex flex-col gap-2">
-        <h1 className="text-xl uppercase font-semibold">Donta enak</h1>
-        <h2>Rp 30000/pcs</h2>
+        <h1 className="text-xl uppercase font-semibold">{product.name}</h1>
+        <h2>Rp {product.price}/pcs</h2>
         <div className="w-min flex border border-black py-1 rounded-full">
-          <button className="w-[40px]">-</button>
-          <p>3</p>
-          <button className="w-[40px]">+</button>
+          <button
+            onClick={() => {
+              dispatch(
+                decrementCart({
+                  id: product.id,
+                  qty: 1,
+                }),
+              );
+            }}
+            className="w-[40px]"
+          >
+            -
+          </button>
+          <p>{qty}</p>
+          <button
+            onClick={() => {
+              dispatch(
+                addToCart({
+                  id: product.id,
+                  qty: 1,
+                }),
+              );
+            }}
+            className="w-[40px]"
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
