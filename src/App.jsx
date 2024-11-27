75c69517660c3a2e54c443e5abbf5b8976a7e1fc
farhanisty <farhannivtasija@gmail.com>
2024-11-27 21:45:59 +0700

    add checkout page to route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About.jsx";
 import Shop from "./pages/Shop.jsx";
 import Contact from "./pages/Contact.jsx";
 import Menu from "./pages/Menu.jsx";
+import Checkout from "./pages/Checkout.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/menu/:id" element={<Menu />} />
       </Routes>
     </Router>
