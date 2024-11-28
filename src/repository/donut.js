34093524b7115e393f0c6a0385e5e243bac68dff
farhanisty <farhannivtasija@gmail.com>
2024-11-28 21:07:23 +0700

    create function to handle payment

--- src/repository/donut.js
+++ src/repository/donut.js
@@ -6,3 +6,22 @@ export async function getDonuts() {
   const response = await axios.get(`${baseApi}/menu`);
   return response.data.data;
 }
+
+export async function createPayment({ name, email, orders }) {
+  const baseApi = import.meta.env.VITE_BASE_API;
+
+  const response = await axios({
+    method: "POST",
+    url: `${baseApi}/payment`,
+    headers: {
+      "content-type": "application/json",
+    },
+    data: {
+      name,
+      email,
+      orders,
+    },
+  });
+
+  return response.data;
+}
